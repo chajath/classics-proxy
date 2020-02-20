5ac14699e8382b5f3d7916c2c11f85e90ed7ef2a
In-Ho Yi <chajath@gmail.com>
2020-02-20 07:23:08 -0500

    Add support for multiple "series" within ITKC corpora:
    BT = "고전 번역서"
    MO = "한국문집총간"
    
    Also add 신출한자 img tag matching and conversion to unicode. We will manually add code lookups as we discover the use of KC code for now.

--- py/server/server.py
+++ py/server/server.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_caching import Cache
 from flask_marshmallow import Marshmallow, fields
 import requests
-from lxml import html
+from lxml import html, etree
 import re
 
 config = {
@@ -50,16 +50,35 @@ def corpora():
 
 
 TITLE_TO_KO_CN = re.compile("(^.*)\\((.*)\\)$")
+SINCHUL_HANJA_LOOKUP = {
+    "KC01783": "楸",
+}
+SINCHUL_CODE_EXTRACT = re.compile(".*(KC[0-9]+).*")
 
-
-@cache.cached(timeout=3600, key_prefix="itkc_collections")
-def get_all_itkc_collections():
+@cache.memoize()
+def get_all_itkc_collections(series_id):
     r = requests.get(
-        "http://db.itkc.or.kr/dir/treeAjax?grpId=&itemId=BT&gubun=book&depth=1"
+        f"http://db.itkc.or.kr/dir/treeAjax?grpId=&itemId={series_id}&gubun=book&depth=1"
     )
     tt = r.text
     tree = html.fromstring(tt)
-    raw_titles = tree.xpath("//li/span/text()")
+    raw_title_spans = tree.xpath("//li/span")
+    raw_titles = []
+    for n in raw_title_spans:
+        t = ""
+        for elem in n.xpath("node()"):
+            if isinstance(elem, str):
+                t = t + elem
+            elif isinstance(elem, html.HtmlElement):
+                if elem.tag == 'img' and elem.attrib['class'] == 'newchar':
+                    code = SINCHUL_CODE_EXTRACT.match(elem.attrib['src']).groups()[0]
+                    if code in SINCHUL_HANJA_LOOKUP:
+                        t = t + SINCHUL_HANJA_LOOKUP.get(code)
+                    else:
+                        print(f"Unknown Sinchul Hanja code: {code}")
+                else:
+                    print(f"Unknown img tag: {etree.tostring(elem,encoding='unicode')}")
+        raw_titles.append(t)
     authors = [t.split(" | ")[1] for t in tree.xpath("//li/span/@title")]
     data_id = tree.xpath("//li/@data-dataid")
     ko_titles, cn_titles = zip(*[TITLE_TO_KO_CN.match(t).groups() for t in raw_titles])
@@ -71,14 +90,19 @@ def get_all_itkc_collections():
 
 @app.route("/corpora/itkc")
 def itkc_root():
-    collections = get_all_itkc_collections()
+    return {"series": ["BT", "MO"]}
+
+
+@app.route("/corpora/itkc/<string:series_id>")
+def itkc_series(series_id):
+    collections = get_all_itkc_collections(series_id)
     return {"collections": collections}
 
 
 @cache.memoize()
-def get_all_itkc_links(depth, data_id):
+def get_all_itkc_links(series_id, depth, data_id):
     r = requests.get(
-        f"http://db.itkc.or.kr/dir/treeAjax?grpId=&itemId=BT&gubun=book&depth={depth}&dataId={data_id}"
+        f"http://db.itkc.or.kr/dir/treeAjax?grpId=&itemId={series_id}&gubun=book&depth={depth}&dataId={data_id}"
     )
     tt = r.text
     tree = html.fromstring(tt)
@@ -94,15 +118,15 @@ ITKC_DEPTH_VOLUME = 2
 ITKC_DEPTH_SECTION = 3
 
 
-@app.route("/corpora/itkc/<string:data_id>")
-def itkc_volumes(data_id):
-    volumes = get_all_itkc_links(ITKC_DEPTH_VOLUME, data_id)
+@app.route("/corpora/itkc/<string:series_id>/<string:data_id>")
+def itkc_volumes(series_id, data_id):
+    volumes = get_all_itkc_links(series_id, ITKC_DEPTH_VOLUME, data_id)
     return {"volumes": volumes}
 
 
-@app.route("/corpora/itkc/<string:book_id>/<string:data_id>")
-def itkc_sections(book_id, data_id):
-    sections = get_all_itkc_links(ITKC_DEPTH_SECTION, data_id)
+@app.route("/corpora/itkc/<string:series_id>/<string:book_id>/<string:data_id>")
+def itkc_sections(series_id, book_id, data_id):
+    sections = get_all_itkc_links(series_id, ITKC_DEPTH_SECTION, data_id)
     return {"sections": sections}
 
 
